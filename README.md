da8a603d1aaac2e3eeb34c4c46c46c7a93ab73af
Nikita Korotkih <nkorotkikh@alfabank.ru>
2016-10-01 13:12:26 +0300

    done

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Axios vs Superagent
+# HTTP libraries performance test
 
+My results:
+```bash
+➜  http-libraries-perf-test git:(master) ✗ node index.js
+http.request POST request x 10,968 ops/sec ±1.99% (73 runs sampled)
+http.request GET request x 11,110 ops/sec ±1.37% (75 runs sampled)
+axios GET request x 3,416 ops/sec ±2.05% (73 runs sampled)
+axios POST request x 3,387 ops/sec ±2.50% (71 runs sampled)
+superagent GET request x 6,498 ops/sec ±2.22% (70 runs sampled)
+superagent POST request x 6,418 ops/sec ±2.80% (71 runs sampled)
+Request GET request x 3,251 ops/sec ±2.85% (71 runs sampled)
+Request POST request x 2,664 ops/sec ±2.87% (67 runs sampled)
+Fastest is http.request GET request,http.request POST request
+```
+
+I'm not suprised that bare `http.request` is the fastest. But I can't explain why `axios` and `request` are so slow.
--- index.js
+++ index.js
@@ -1,41 +1,81 @@
-var Benchmark = require('benchmark');
-var suite = new Benchmark.Suite;
-var createServer = require('./server').create;
-// var PORT = require('./server').PORT;
 var http = require('http');
+var axios = require('axios');
+var superagent = require('superagent');
+var request = require('request');
 
 var nock = require('nock');
 var HOST = 'test-perf';
-var axios = require('axios');
-axios.defaults.baseURL = `http://${HOST}`,
+
+axios.defaults.baseURL = `http://${HOST}`;
+
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite;
 
 nock('http://test-perf').persist()
-    .log(console.log)
+    // .log(console.log)
     .post('/test').reply(200, 'ok')
     .get('/test').reply(200, 'ok');
 
-// suite.add('POST request', (defer) => {
-//     var req = http.request({ host: HOST, path: '/test', method: 'POST' }, function(res) {
-//         res.on('end', () => {
-//             defer.resolve();
-//         });
-//     });
-//     req.end();
-// });
+suite.add('http.request POST request', {
+    defer: true,
+    fn: (defer) => {
+        var req = http.request({ host: HOST, path: '/test', method: 'POST' }, (res) => {
+            res.resume().on('end', () => defer.resolve());
+        });
+        req.write();
+        req.end();
+    }
+});
 
-// suite.add('GET request', (defer) => {
-//     var req = http.request({ path: '/test', host: HOST }, function(res) {
-//         res.on('end', () => {
-//             defer.resolve();
-//         });
-//     });
-//     req.end();
-// });
+suite.add('http.request GET request', {
+    defer: true,
+    fn: (defer) => {
+        http.request({ path: '/test', host: HOST }, (res) => {
+            res.resume().on('end', () => defer.resolve());
+        }).end();
+    }
+});
 
-suite.add('GET request with axios', defer => {
-    axios.get('/test')
-        .then(() => { console.log('here'); defer.resolve(); }, (e) => console.log)
-        .catch((e) => { console.log(e); });
+suite.add('axios GET request', {
+    defer: true,
+    fn: (defer) => {
+        axios.get('/test').then(() => defer.resolve())
+    }
+});
+
+suite.add('axios POST request', {
+    defer: true,
+    fn: (defer) => {
+        axios.post('/test').then(() => defer.resolve());
+    }
+});
+
+suite.add('superagent GET request', {
+    defer: true,
+    fn: (defer) => {
+        superagent.get(`http://${HOST}/test`).end(() => { defer.resolve(); });
+    }
+});
+
+suite.add('superagent POST request', {
+    defer: true,
+    fn: (defer) => {
+        superagent.post(`http://${HOST}/test`).send().end(() => defer.resolve());
+    }
+});
+
+suite.add('Request GET request', {
+    defer: true,
+    fn: (defer) => {
+        request(`http://${HOST}/test`, () => defer.resolve());
+    }
+});
+
+suite.add('Request POST request', {
+    defer: true,
+    fn: (defer) => {
+        request.post({ url: `http://${HOST}/test` }, () => defer.resolve());
+    }
 });
 
 suite.on('complete', function(defer) {
@@ -46,4 +86,4 @@ suite.on('cycle', function(event) {
     console.log(String(event.target));
 });
 
-suite.run({ defer: true, maxTime: 3 });
+suite.run({ async: true });
